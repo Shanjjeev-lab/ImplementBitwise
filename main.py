af7ebee4bc2291bef205c3564f2b771f7b6a7553
Shanjjeev-Lab <shanjjeevrohan2911@gmail.com>
2024-12-05 18:47:02 +0530

    Python BitOperators Activity

---
+++ main.py
@@ -0,0 +1,37 @@
+num = 8
+print("The binary of", num, "is", bin(num))
+not_num = ~num
+print("The not of", num, "is", not_num)
+print("The binary of", not_num, "is", bin(not_num))
+
+
+a = 8
+b = 7
+print("Bin of a", bin(a))
+print("Bin of b", bin(b))
+print("a and b =", a & b)
+print("Bin of a and b =", bin(a & b))
+
+
+a = 8
+b = 7
+print("Bin of a", bin(a))
+print("Bin of b", bin(b))
+print("a or b =", a | b)
+print("Bin of a or b =", bin(a | b))
+
+
+a = 8
+b = 7
+print("Bin of a", bin(a))
+print("Bin of b", bin(b))
+print("a xor b =", a ^ b)
+print("Bin of a xor b =", bin(a ^ b))
+
+
+a = 7
+print("Left Shift", a << 1)
+
+
+a = 12
+print("Right Shift", a >> 1)
